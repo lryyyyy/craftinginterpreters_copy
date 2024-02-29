2330e4f693047d59116c0499b7cc1bfce99e1d81
lryyyyy <restlesssh@163.com>
2024-02-29 20:53:43 +0800

    02 A_Virtual Machine append

--- 02_A_Virtual_Machine/main.c
+++ 02_A_Virtual_Machine/main.c
@@ -1,15 +1,28 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+  InitVM();
   Chunk chunk;
   InitChunk(&chunk);
   int constant = AddConstant(&chunk, 1.2);
   WriteChunk(&chunk, OP_CONSTANT, 1);
   WriteChunk(&chunk, constant, 1);
+  constant = AddConstant(&chunk, 3.4);
+  WriteChunk(&chunk, OP_CONSTANT, 1);
+  WriteChunk(&chunk, constant, 1);
+  WriteChunk(&chunk, OP_ADD, 1);
+  constant = AddConstant(&chunk, 5.6);
+  WriteChunk(&chunk, OP_CONSTANT, 1);
+  WriteChunk(&chunk, constant, 1);
+  WriteChunk(&chunk, OP_DIVIDE, 1);
+  WriteChunk(&chunk, OP_NEGATE, 1);
   WriteChunk(&chunk, OP_RETURN, 1);
   DisassembleChunk(&chunk, "test chunk");
+  Interpret(&chunk);
   FreeChunk(&chunk);
+  FreeVM();
   return 0;
 }
--- 02_A_Virtual_Machine/src/debug.c
+++ 02_A_Virtual_Machine/src/debug.c
@@ -29,10 +29,20 @@ int DisassembleInstruction(Chunk *chunk, int offset) {
 
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
-  case OP_RETURN:
-    return SimpleInstruction("OP_RETURN", offset);
   case OP_CONSTANT:
     return ConstantInstruction("OP_CONSTANT", chunk, offset);
+  case OP_ADD:
+    return SimpleInstruction("OP_ADD", offset);
+  case OP_SUBTRACT:
+    return SimpleInstruction("OP_SUBTRACT", offset);
+  case OP_MULTIPLY:
+    return SimpleInstruction("OP_MULTIPLY", offset);
+  case OP_DIVIDE:
+    return SimpleInstruction("OP_DIVIDE", offset);
+  case OP_NEGATE:
+    return SimpleInstruction("OP_NEGATE", offset);
+  case OP_RETURN:
+    return SimpleInstruction("OP_RETURN", offset);
   default:
     printf("Unknown opcode %d\n", instruction);
     return offset + 1;
